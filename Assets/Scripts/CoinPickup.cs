3e44abbf9cdd651363cb348659014b95338cf3e7
Youssef <ykawkgi@gmail.com>
2025-10-23 11:22:00 -0400

    Initial commit

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int pointsForCoinPickip = 100;
+
+    bool wasCollected = false;
+    void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player" && !wasCollected)
+            {
+                wasCollected = true;
+                FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickip);
+                AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+                gameObject.SetActive(false);
+                Destroy(gameObject);
+            }
+        }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    Rigidbody2D myRigidbody;
+    [SerializeField] float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+
+}
